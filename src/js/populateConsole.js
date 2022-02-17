9a61ced7903f5c77fbcea37cb06fbb4c7e29d435
bradleygichuru <prz03@protonmail.com>
2022-02-17 21:47:57 +0300

    fixed cross origin error

--- src/js/populateConsole.js
+++ src/js/populateConsole.js
@@ -1,15 +1,14 @@
 //TODO populate console with user passwords
+//TODO add functionlaity to add passwords 
 
 async function populate(){
     let token = document.cookie.split("=")[1]
-    let url = `${document.location.host}/api/${token}`
+    let home = "https://pswm0.herokuapp.com"
+    let url = `${home}/api/${token}`
     console.log({requestUrl:url})
-    let data = await fetch(url,{
-      mode:'cors'
-    })
-      
+    let data = await fetch(url)
       console.log(await data.json())
 
 }
-//TODO fix cross origin request error 
+
 populate()
