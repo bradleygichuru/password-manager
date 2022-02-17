217e8907efbdbd4133a25f087015b4d35f6c41e2
bradleygichuru <prz03@protonmail.com>
2022-02-17 21:43:50 +0300

    added post route to add passwords

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express, { json } from "express";
 import path from "path";
 import {
   checkIfUserExists,
-  addUser, retrieveUserData
+  addUser, retrieveUserData,addPassword
 } from "./src/js/pipeline.js";
 //const path = require("path");
 //const fs = require("fs");
+//TODO write test for github CI
 var dirname = path.resolve("./");
 console.log(dirname);
 import cookieParser from "cookie-parser";
@@ -36,10 +37,12 @@ server.use(express.static(staticDir));
 console.log(dirname);
 
 server.get("/", (req, res) => {
-  //TODO if cookie exists send user direct to console 
+  
   if (req.cookies['authtoken']) {
     console.log({ cookie: req.cookies['authtoken'] })//debug logs
-    res.redirect(`/console/${req.cookies['authtoken']}`)
+    res.
+    status(200)
+    redirect(`/console/${req.cookies['authtoken']}`)
   }
   else{
   console.log(req.url);
@@ -66,7 +69,6 @@ server.get("/access", (req, res) => {
 server.get("/console/:identifier", async (req, res) => {
   console.log(req.url);
   
-  //TODO functionality to display user passwords in console.html
   res
     .status(200)
     .type(".html")
@@ -101,13 +103,14 @@ server.post("/auth", async (req, res) => {
       console.log({ usertoken: data.token })//debug log
       res
         .cookie('authtoken', data.token)
+        .status(200)
         .redirect(`/console/${data.token}`)
 
       //console.log({ id: existence._id })//debug log
     } else if (!existence) {
       console.log("user does not exist"); //DEBUG log 
 
-      //TODO add login logic and intergrate with db
+      
 
       let token = generateToken(
         req.body.password + req.body.username,
@@ -133,6 +136,18 @@ server.post("/auth", async (req, res) => {
     res.status(404).type(".html").send("error");
   }
 });
+server.post("/addpswd",async (req,res)=>{
+  if (req.cookies['authtoken']) {
+    let token = req.cookies['authtoken'] 
+    addPassword(token,{site:req.body.siteurl,username:req.body.username,password:req.body.password},token)
+    res
+      .status(200)
+      .redirect(`/console/${token}`)
+  }
+  else{
+    res.redirect('/access')
+  }
+})
 
 
 server.listen(port, () => {
