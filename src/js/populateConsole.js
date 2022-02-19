f13d38a9b26bfd0cd9bbe9b89c8399983e440268
bradleygichuru <prz03@protonmail.com>
2022-02-20 00:52:21 +0300

    populates passwords

--- src/js/populateConsole.js
+++ src/js/populateConsole.js
@@ -1,13 +1,25 @@
-//TODO populate console with user passwords
-//TODO add functionlaity to add passwords 
 
-async function populate(){
-    let token = document.cookie.split("=")[1]
-    let home = "https://pswm0.herokuapp.com"
-    let url = `${home}/api/${token}`
-    console.log({requestUrl:url})
-    let data = await fetch(url)
-      console.log(await data.json())
+
+
+async function populate() {
+  let token = document.cookie.split("=")[1]
+  let home = "https://pswm0.herokuapp.com"
+  let url = `${home}/api/${token}`
+  console.log({ requestUrl: url })
+  let data = await (await fetch(url)).json()
+  //data = await data.json()
+  data = JSON.parse(data)
+  console.log(data['payload'])//DEBUG log 
+  
+  data.payload.forEach(element => {
+    //console.log(element)
+    let container = document.getElementById('passwords-container')
+    let password = document.createElement('div');
+    password.className = 'password'
+    password.innerHTML = `site : ${element['site']}`
+    container.appendChild(password)
+  });
+
 
 }
 
